10e78ed77383de57cac122821233fd8c4f441f8a
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-10-30 13:44:39 +0100

    asedfcgv

--- clases/figuras.py
+++ clases/figuras.py
@@ -3,6 +3,4 @@ class Figura:
         self.figuras = []
 
     def __str__(self):
-        # Representación genérica de una figura: nombre de la clase y atributos públicos
-        attrs = {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
-        return f"{self.__class__.__name__}({', '.join(f'{k}: {v}' for k, v in attrs.items())})"
+        return f'Figura con {len(self.figuras)} figuras.'
