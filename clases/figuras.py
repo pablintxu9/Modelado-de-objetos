f4b0e0bfb304b85427f0cbd746539601115c8640
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-10-30 13:39:11 +0100

    cor1

--- clases/figuras.py
+++ clases/figuras.py
@@ -6,4 +6,3 @@ class Figura:
         # Representación genérica de una figura: nombre de la clase y atributos públicos
         attrs = {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
         return f"{self.__class__.__name__}({', '.join(f'{k}: {v}' for k, v in attrs.items())})"
-
