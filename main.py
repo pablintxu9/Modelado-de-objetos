57c171a8c0593f4c8088045225a7e381498b6abb
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-10-30 13:28:27 +0100

    fin

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from clases.circulo import Circulo
+from clases.rectangulo import Rectangulo
+from clases.cuadrado import Cuadrado
+from clases.elipse import Elipse
 
 
 def main():
 	# Generar un círculo y imprimirlo (color por defecto: 'negro')
-	c = Circulo(1)  # radio 5, color por defecto 'negro'
-	print("el color del circulo es ",c)
+	c = Circulo(5)           # Circulo: color por defecto 'negro'
+	r = Rectangulo(3, 1)    # Rectangulo: color por defecto 'naranja'
+	q = Cuadrado(1.5)         # Cuadrado: color por defecto 'azul'
+	e = Elipse(1, 3)        # Elipse: color por defecto 'amarillo'
+
+	print(c)
+	print(r)
+	print(q)
+	print(e)
 
 
 if __name__ == '__main__':
