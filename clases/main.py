a8d7be7af0b7fe693dde765f6aadab31b0b80a80
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-11-13 13:00:49 +0100

    wjer 1

--- clases/main.py
+++ clases/main.py
@@ -4,12 +4,11 @@ from clases.cuadrado import Cuadrado
 from clases.elipse import Elipse
 
 
-def main():
-	# Generar un círculo y imprimirlo (color por defecto: 'negro')
-	c = Circulo(1,"negro")           # Circulo: color por defecto 'negro'
-	r = Rectangulo(3, 1, "naranja")    # Rectangulo: color por defecto 'naranja'
-	q = Cuadrado(1.5, "azul")         # Cuadrado: color por defecto 'azul'
-	e = Elipse(1, 3, "amarillo")        # Elipse: color por defecto 'amarillo'
+if __name__ == "__main__":
+	c = Circulo(1,"negro")         
+	r = Rectangulo(3, 1, "naranja")    
+	q = Cuadrado(1.5, "azul")        
+	e = Elipse(1, 3, "amarillo")    
 
 	print(c)
 	print(r)
